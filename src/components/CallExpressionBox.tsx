e97e40c840c1622c65e5ebc5a267090e0b1bf134
Shu Uesugi <shu@chibicode.com>
2018-08-01 08:42:52 -0700

    Remove unnecessary brackets

--- src/components/CallExpressionBox.tsx
+++ src/components/CallExpressionBox.tsx
@@ -27,38 +27,34 @@ const stateToArrowsBgColor = (x: CallExpressionStates): string | undefined => {
 
 const CallExpressionBox: React.SFC<CallProps> = ({ expression }) => (
   <Flex flexDirection="column" flex={1}>
-    {
-      <Flex
-        justifyContent="center"
-        borderBottom={2}
-        borderColor="lightGray"
-        color="lightGray"
-        bg={stateToArrowsBgColor(expression.state)}
-      >
-        {times(expression.priority, () => (
-          <FontAwesomeIcon icon="angle-down" />
-        ))}
-      </Flex>
-    }
+    <Flex
+      justifyContent="center"
+      borderBottom={2}
+      borderColor="lightGray"
+      color="lightGray"
+      bg={stateToArrowsBgColor(expression.state)}
+    >
+      {times(expression.priority, () => (
+        <FontAwesomeIcon icon="angle-down" />
+      ))}
+    </Flex>
     <Flex justifyContent="center" borderBottom={2} borderColor="lightGray">
       <ExpressionBox expression={expression.arg} />
     </Flex>
     <Flex justifyContent="center" borderBottom={2} borderColor="lightGray">
       <ExpressionBox expression={expression.func} />
     </Flex>
-    {
-      <Flex
-        justifyContent="center"
-        borderColor="lightGray"
-        width={1}
-        color="lightGray"
-        bg={stateToArrowsBgColor(expression.state)}
-      >
-        {times(expression.priority, () => (
-          <FontAwesomeIcon icon="angle-up" />
-        ))}
-      </Flex>
-    }
+    <Flex
+      justifyContent="center"
+      borderColor="lightGray"
+      width={1}
+      color="lightGray"
+      bg={stateToArrowsBgColor(expression.state)}
+    >
+      {times(expression.priority, () => (
+        <FontAwesomeIcon icon="angle-up" />
+      ))}
+    </Flex>
   </Flex>
 )
 
