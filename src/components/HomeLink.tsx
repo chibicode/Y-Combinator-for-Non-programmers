949d5f442f82fb03d582192c3ddcad0564f8b84c
Shu Uesugi <shu@chibicode.com>
2018-09-23 21:28:54 +0900

    Add HomeLink component

---
+++ src/components/HomeLink.tsx
@@ -0,0 +1,33 @@
+import { css } from 'emotion'
+import Link, { LinkState } from 'next/link'
+import React from 'react'
+import { colors, fontWeights } from 'src/lib/theme'
+
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+
+interface HomeLink {
+  linkProps: Omit<LinkState, 'children'>
+  anchorProps?: React.HTMLAttributes<HTMLAnchorElement>
+  children: React.ReactNode
+}
+
+const HomeLink: React.SFC<HomeLink> = ({
+  children,
+  linkProps,
+  anchorProps
+}) => (
+  <Link {...linkProps}>
+    <a
+      {...anchorProps}
+      className={css`
+        font-weight: ${fontWeights(800)};
+        color: ${colors('pink400')};
+        text-decoration: underline;
+      `}
+    >
+      {children}
+    </a>
+  </Link>
+)
+
+export default HomeLink
