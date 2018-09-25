6fb457770a97f5f85f204f7fcb3e9af34c34c74d
Shu Uesugi <shu@chibicode.com>
2018-09-25 20:08:35 +0900

    Design hr tag

--- src/components/ContentTags/Hr.tsx
+++ src/components/ContentTags/Hr.tsx
@@ -1,5 +1,21 @@
+import { css, cx } from 'emotion'
 import React from 'react'
+import { colors } from 'src/lib/theme'
 
 export const Hr: React.SFC<
   React.DetailedHTMLProps<React.HTMLAttributes<HTMLHRElement>, HTMLHRElement>
-> = props => <hr {...props} />
+> = ({ className, ...props }) => (
+  <hr
+    {...props}
+    className={cx(
+      className,
+      css`
+        border-top: none;
+        border-bottom: 1px solid ${colors('grey300')};
+        margin-top: 2em;
+        margin-bottom: 2em;
+        width: 25%;
+      `
+    )}
+  />
+)
