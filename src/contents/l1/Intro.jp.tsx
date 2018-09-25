c3a1c057a3b9b48928cdf9769afe76be3f8911bd
Shu Uesugi <shu@chibicode.com>
2018-09-26 00:32:34 +0900

    Japanese intro

--- src/contents/l1/Intro.jp.tsx
+++ src/contents/l1/Intro.jp.tsx
@@ -1,9 +1,37 @@
+import {
+  Em,
+  Hr,
+  Ol,
+  OlLi,
+  P,
+  Strong,
+  Ul,
+  UlLi
+} from 'src/components/ContentTags'
+
 export default () => (
   <>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam minima
-      incidunt cupiditate nam, id, voluptate maxime ut dolor aut saepe mollitia
-      repellendus enim tempora amet iure vitae similique culpa? Rerum!
-    </p>
+    <P>
+      <Strong>クイズです。</Strong>「
+      <Strong>
+        <Em>Yコンビネーター</Em>
+      </Strong>
+      」という単語の意味として正しいのはどれでしょう？
+    </P>
+    <Ol>
+      <OlLi>
+        スタートアップ企業に投資し育成する、シリコンバレー発の
+        <Em>スタートアップ養成スクール</Em>。
+      </OlLi>
+      <OlLi>
+        <Em>再帰関数を定義する手段</Em>
+        のひとつ。
+      </OlLi>
+      <OlLi>
+        <Em>不動点コンビネータ</Em>
+        の一種。
+      </OlLi>
+    </Ol>
+    <Hr />
   </>
 )
