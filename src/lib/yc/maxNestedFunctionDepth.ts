022b82b2f0a8f320a7dd208696430d3ac6c8a0e0
Shu Uesugi <shu@chibicode.com>
2018-10-01 17:17:57 +0900

    maxNestedFunctionDepth

---
+++ src/lib/yc/maxNestedFunctionDepth.ts
@@ -0,0 +1,18 @@
+import {
+  Expression,
+  isCallExpression,
+  isVariableExpression
+} from 'src/types/yc/ExpressionTypes'
+
+export default function maxNestedFunctionDepth(expression: Expression): number {
+  if (isVariableExpression(expression)) {
+    return 0
+  } else if (isCallExpression(expression)) {
+    return Math.max(
+      maxNestedFunctionDepth(expression.arg),
+      maxNestedFunctionDepth(expression.func)
+    )
+  } else {
+    return 1 + maxNestedFunctionDepth(expression.body)
+  }
+}
