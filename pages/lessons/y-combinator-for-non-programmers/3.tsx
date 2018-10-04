397ade3a2ee9089b105fa0f5793fcf1347c73d23
Shu Uesugi <shu@chibicode.com>
2018-10-04 10:55:01 +0900

    Add better emoji loader

--- pages/lessons/y-combinator-for-non-programmers/3.tsx
+++ pages/lessons/y-combinator-for-non-programmers/3.tsx
@@ -4,7 +4,7 @@ import EpisodePageWrapper from 'src/components/EpisodePageWrapper'
 const Episode3: React.SFC<{}> = () => (
   <EpisodePageWrapper
     episodeNumber={3}
-    emojis={['😋', '🍽', '👨‍🍳']}
+    emojis={['⏪', '🤔', '⏩']}
     lessonName="yc"
   />
 )
---
+++ src/components/Twemoji/2b1c.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Svg2b1c = props => (
+  <svg viewBox="0 0 36 36" {...props}>
+    <path
+      fill="#E6E7E8"
+      d="M36 32a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4h28a4 4 0 0 1 4 4v28z"
+    />
+  </svg>
+)
+
+export default Svg2b1c
