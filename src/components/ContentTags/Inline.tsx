8794c42f1e44898404384d4f1cecafc456c57093
Shu Uesugi <shu@chibicode.com>
2018-10-14 02:47:25 -0700

    Undo strong

--- src/components/ContentTags/Inline.tsx
+++ src/components/ContentTags/Inline.tsx
@@ -3,19 +3,8 @@ import React from 'react'
 import SectionContext from 'src/components/SectionContext'
 import { colors } from 'src/lib/theme'
 
-export const Strong: React.SFC<JSX.IntrinsicElements['strong']> = ({
-  className,
-  ...props
-}) => (
-  <strong
-    {...props}
-    className={cx(
-      css`
-        colors: ${colors('grey900')};
-      `,
-      className
-    )}
-  />
+export const Strong: React.SFC<JSX.IntrinsicElements['strong']> = props => (
+  <strong {...props} />
 )
 
 export const Em: React.SFC<JSX.IntrinsicElements['em']> = ({
