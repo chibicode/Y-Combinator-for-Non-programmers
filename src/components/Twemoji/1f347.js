9dc76e096695d9bcef859400847b5b829f2a10d4
Shu Uesugi <shu@chibicode.com>
2018-10-21 06:51:54 -0700

    Add all food emojis

---
+++ src/components/Twemoji/1f347.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Svg1f347 = props => (
+  <svg viewBox="0 0 36 36" {...props}>
+    <path
+      fill="#77B255"
+      d="M9.999 12a.99.99 0 0 1-.446-.106c-4.38-2.19-7.484-7.526-8.501-10.578A.999.999 0 1 1 2.949.683c.877 2.632 3.688 7.517 7.499 9.422A1 1 0 0 1 9.999 12z"
+    />
+    <circle fill="#553788" cx={19} cy={29} r={7} />
+    <circle fill="#9266CC" cx={10} cy={15} r={7} />
+    <circle fill="#AA8DD8" cx={19} cy={12} r={7} />
+    <circle fill="#744EAA" cx={27} cy={18} r={7} />
+    <circle fill="#744EAA" cx={9} cy={26} r={7} />
+    <circle fill="#9266CC" cx={18} cy={21} r={7} />
+    <circle fill="#9266CC" cx={29} cy={29} r={7} />
+  </svg>
+)
+
+export default Svg1f347
