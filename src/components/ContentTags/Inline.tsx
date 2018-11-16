55a6873d3244cd2a8c03c6dc3d74b50494e299b5
Shu Uesugi <shu@chibicode.com>
2018-11-16 15:17:37 +0900

    Fix css for strong

--- src/components/ContentTags/Inline.tsx
+++ src/components/ContentTags/Inline.tsx
@@ -3,8 +3,19 @@ import React from 'react'
 import SectionContext from 'src/components/SectionContext'
 import { colors } from 'src/lib/theme'
 
-export const Strong: React.SFC<JSX.IntrinsicElements['strong']> = props => (
-  <strong {...props} />
+export const Strong: React.SFC<JSX.IntrinsicElements['strong']> = ({
+  className,
+  ...props
+}) => (
+  <strong
+    className={cx(
+      css`
+        font-weight: bold;
+      `,
+      className
+    )}
+    {...props}
+  />
 )
 
 export const Em: React.SFC<JSX.IntrinsicElements['em']> = ({
