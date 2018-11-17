494977f1832b3ad47b2ca932d336680ccc43b69b
Shu Uesugi <shu@chibicode.com>
2018-11-17 08:27:23 +0700

    Allow touch

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -5,42 +5,21 @@ import GlobalContextProvider from 'src/components/GlobalContextProvider'
 import GlobalStyles from 'src/components/GlobalStyles'
 import t from 'src/lib/t'
 
-class Page extends React.Component<{ children: React.ReactNode }> {
-  private lastTouchEnd = 0
-
-  public componentDidMount() {
-    document.addEventListener(
-      'touchend',
-      event => {
-        const now = new Date().getTime()
-        if (now - this.lastTouchEnd <= 300) {
-          event.preventDefault()
-        }
-        this.lastTouchEnd = now
-      },
-      false
-    )
-  }
-
-  public render() {
-    const { children } = this.props
-    return (
-      <GlobalContextProvider>
-        <GlobalStyles>
-          <Head>
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1, shrink-to-fit=no"
-              key="viewport"
-            />
-            <title key="title">{t('title')}</title>
-            <Favicon />
-          </Head>
-          {children}
-        </GlobalStyles>
-      </GlobalContextProvider>
-    )
-  }
-}
+const Page: React.SFC<{ children: React.ReactNode }> = ({ children }) => (
+  <GlobalContextProvider>
+    <GlobalStyles>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          key="viewport"
+        />
+        <title key="title">{t('title')}</title>
+        <Favicon />
+      </Head>
+      {children}
+    </GlobalStyles>
+  </GlobalContextProvider>
+)
 
 export default Page
