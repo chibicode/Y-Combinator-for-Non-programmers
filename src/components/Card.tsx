97b6833107cd562574b501491c23060d8b44eb02
Shu Uesugi <shu@chibicode.com>
2019-02-19 02:09:18 -0800

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,8 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core'
 export const jsxBabelFix = jsx
-import React from 'react'
+import React, { useState } from 'react'
 import CardContext from 'src/components/CardContext'
-import { CardContextProps } from 'src/components/CardContext'
 import locale from 'src/lib/locale'
 import { colors, fontSizes, ns, radii, spaces } from 'src/lib/theme'
 
@@ -45,96 +44,94 @@ const emBackgroundColor = (color: CardProps['color']) =>
     indigo: colors('white')
   }[color])
 
-export default class Card extends React.Component<CardProps, CardState> {
-  public static defaultProps = {
-    color: 'white'
-  }
+const Card = ({ children, color, slideNumber, slideCount }: CardProps) => {
+  const [overrideColor, setOverrideColor] = useState<
+    CardProps['color'] | undefined
+  >(undefined)
+  const finalColor = overrideColor || color
+  const updateColor = (newColor: CardProps['color']) =>
+    setOverrideColor(newColor)
+  return (
+    <>
+      <CardContext.Provider
+        value={{
+          emBackgroundColor: emBackgroundColor(finalColor),
+          updateColor
+        }}
+      >
+        <div
+          css={css`
+            padding-top: ${spaces(1)};
+            padding-left: ${spaces(1)};
+            padding-right: ${spaces(1)};
+            padding-bottom: ${spaces(0.5)};
 
-  public state: CardState = {}
-
-  public render() {
-    const { children, color, slideNumber, slideCount } = this.props
-    const finalColor = this.state.overrideColor || color
-    return (
-      <>
-        <CardContext.Provider
-          value={{
-            emBackgroundColor: emBackgroundColor(finalColor),
-            updateColor: this.updateColor
-          }}
+            ${ns} {
+              padding-top: ${spaces(2)};
+              padding-left: ${spaces(2)};
+              padding-right: ${spaces(2)};
+              padding-bottom: ${spaces(1.5)};
+            }
+            position: relative;
+            background: ${backgroundColor(finalColor)};
+            border-radius: ${radii(0.5)};
+            margin-bottom: ${slideNumber === undefined ? spaces(1.5) : 0};
+          `}
         >
-          <div
-            css={css`
-              padding-top: ${spaces(1)};
-              padding-left: ${spaces(1)};
-              padding-right: ${spaces(1)};
-              padding-bottom: ${spaces(0.5)};
-
-              ${ns} {
-                padding-top: ${spaces(2)};
-                padding-left: ${spaces(2)};
-                padding-right: ${spaces(2)};
-                padding-bottom: ${spaces(1.5)};
-              }
-              position: relative;
-              background: ${backgroundColor(finalColor)};
-              border-radius: ${radii(0.5)};
-              margin-bottom: ${slideNumber === undefined ? spaces(1.5) : 0};
-            `}
-          >
-            {slideNumber && slideCount && (
-              <div
-                css={css`
-                  position: absolute;
-                  bottom: -0.5rem;
-                  right: 1rem;
-                  font-size: ${fontSizes(0.75)};
-                  line-height: 1;
-                  color: ${colors('white')};
-                  background: ${slideLabelBgColor(finalColor)};
-                  padding: ${spaces(0.25)} ${spaces(0.5)};
-                  border-radius: 9999px;
-                `}
-              >
-                <>
-                  {slideNumber === 1 && (
-                    <>{locale === 'jp' ? 'スライド' : 'Slide'} </>
-                  )}
-                  <span
-                    css={css`
-                      font-weight: bold;
-                    `}
-                  >
-                    {slideNumber}
-                  </span>{' '}
-                  <span
-                    css={css`
-                      color: ${colors('white66')};
-                    `}
-                  >
-                    / {slideCount}
-                  </span>
-                </>
-              </div>
-            )}
-            {children}
-          </div>
-        </CardContext.Provider>
-        {slideNumber !== slideCount && (
-          <div
-            css={css`
-              width: 1.25rem;
-              height: 2rem;
-              margin: 0 auto;
-              background: ${colors('indigo200')};
-            `}
-          />
-        )}
-      </>
-    )
-  }
-
-  private updateColor: CardContextProps['updateColor'] = overrideColor => {
-    this.setState({ overrideColor })
-  }
+          {slideNumber && slideCount && (
+            <div
+              css={css`
+                position: absolute;
+                bottom: -0.5rem;
+                right: 1rem;
+                font-size: ${fontSizes(0.75)};
+                line-height: 1;
+                color: ${colors('white')};
+                background: ${slideLabelBgColor(finalColor)};
+                padding: ${spaces(0.25)} ${spaces(0.5)};
+                border-radius: 9999px;
+              `}
+            >
+              <>
+                {slideNumber === 1 && (
+                  <>{locale === 'jp' ? 'スライド' : 'Slide'} </>
+                )}
+                <span
+                  css={css`
+                    font-weight: bold;
+                  `}
+                >
+                  {slideNumber}
+                </span>{' '}
+                <span
+                  css={css`
+                    color: ${colors('white66')};
+                  `}
+                >
+                  / {slideCount}
+                </span>
+              </>
+            </div>
+          )}
+          {children}
+        </div>
+      </CardContext.Provider>
+      {slideNumber !== slideCount && (
+        <div
+          css={css`
+            width: 1.25rem;
+            height: 2rem;
+            margin: 0 auto;
+            background: ${colors('indigo200')};
+          `}
+        />
+      )}
+    </>
+  )
 }
+
+Card.defaultProps = {
+  color: 'white'
+}
+
+export default Card
