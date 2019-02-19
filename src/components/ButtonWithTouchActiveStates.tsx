fd7ef9af91ca7d2b7176d6538e8333d5f606a4da
Shu Uesugi <shu@chibicode.com>
2019-02-19 01:31:48 -0800

    Update ButtonWithTouchActiveStates.tsx

--- src/components/ButtonWithTouchActiveStates.tsx
+++ src/components/ButtonWithTouchActiveStates.tsx
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core'
 export const jsxBabelFix = jsx
-import React from 'react'
+import { useState } from 'react'
 
 type ButtonWithTouchActiveStatesProps<
   T = JSX.IntrinsicElements['button']
@@ -9,46 +9,36 @@ type ButtonWithTouchActiveStatesProps<
   activeBackgroundColor: string
 }
 
-interface ButtonWithTouchActiveStatesState {
-  isActive: boolean
-}
-
-export default class ButtonWithTouchActiveStates extends React.Component<
-  ButtonWithTouchActiveStatesProps,
-  ButtonWithTouchActiveStatesState
-> {
-  public state = {
-    isActive: false
-  }
-
-  public render() {
-    const { activeBackgroundColor, ...props } = this.props
-    return (
-      <button
-        {...props}
-        css={
-          this.state.isActive &&
-          css`
-            background: ${activeBackgroundColor} !important;
-          `
-        }
-        onTouchStart={this.activate}
-        onTouchEnd={this.deactivate}
-        onTouchCancel={this.deactivate}
-      />
-    )
-  }
-
-  private activate = () => {
-    if (!this.props.disabled) {
+const ButtonWithTouchActiveStates = ({
+  activeBackgroundColor,
+  ...props
+}: ButtonWithTouchActiveStatesProps) => {
+  const [isActive, setIsActive] = useState(false)
+  const activate = () => {
+    if (!props.disabled) {
       // NOTE: Originally tried to call the callback here and do e.preventDefault()
       // to prevent mouse click event from happening, so that callback fires
       // on tap start instead of on tap end, but that was buggy so ended up removing.
-      this.setState({ isActive: true })
+      setIsActive(true)
     }
   }
-
-  private deactivate = () => {
-    this.setState({ isActive: false })
+  const deactivate = () => {
+    setIsActive(false)
   }
+  return (
+    <button
+      {...props}
+      css={
+        isActive &&
+        css`
+          background: ${activeBackgroundColor} !important;
+        `
+      }
+      onTouchStart={activate}
+      onTouchEnd={deactivate}
+      onTouchCancel={deactivate}
+    />
+  )
 }
+
+export default ButtonWithTouchActiveStates
