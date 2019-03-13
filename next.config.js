b40c63714fac52102542c34a875e1c243d52082e
Shu Uesugi <shu@chibicode.com>
2019-03-13 04:50:14 -0700

    NODE_ENV

--- next.config.js
+++ next.config.js
@@ -33,7 +33,8 @@ const config = withCSS(
       return config
     },
     env: {
-      productionLocale: process.env.PRODUCTION_LOCALE
+      productionLocale: process.env.PRODUCTION_LOCALE,
+      NODE_ENV: process.env.NODE_ENV
     },
     publicRuntimeConfig: {
       devLocale: process.env.DEV_LOCALE
