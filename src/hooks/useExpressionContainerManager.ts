4386ec27280ef8b76c8882c7aa2a51668524d0aa
Shu Uesugi <shu@chibicode.com>
2019-03-13 20:46:24 -0700

    Update useExpressionContainerManager.ts

--- src/hooks/useExpressionContainerManager.ts
+++ src/hooks/useExpressionContainerManager.ts
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react'
+import { useState, useRef } from 'react'
 import useScrollAdjuster from 'src/hooks/useScrollAdjuster'
 import ExpressionContainerManager from 'src/lib/yc/ExpressionContainerManager'
 import { SteppedExpressionContainer } from 'src/types/yc/ExpressionContainerTypes'
