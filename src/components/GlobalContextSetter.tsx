e77ccefb6abb6f5b0a180f112d86fdd1dd88b61b
Shu Uesugi <shu@chibicode.com>
2019-03-17 21:49:03 -0700

    Fix infinite loop

--- src/components/GlobalContextSetter.tsx
+++ src/components/GlobalContextSetter.tsx
@@ -1,4 +1,3 @@
-import { withRouter, WithRouterProps } from 'next/router'
 import React, { useEffect, useContext } from 'react'
 import GlobalContext from 'src/components/GlobalContext'
 import pathHelpers from 'src/lib/pathHelpers'
@@ -13,22 +12,27 @@ const GlobalContextSetter = ({
   episodeNumber,
   lessonName,
   children
-}: GlobalContextProviderProps & WithRouterProps) => {
-  const { furthestEpisodes, setFurthestEpisodes } = useContext(GlobalContext)
+}: GlobalContextProviderProps) => {
+  const { setFurthestEpisodes } = useContext(GlobalContext)
   useEffect(() => {
-    const furthestEpisodeForThisLesson = furthestEpisodes[lessonName]
-    if (
-      !furthestEpisodeForThisLesson ||
-      (typeof furthestEpisodeForThisLesson !== 'undefined' &&
-        furthestEpisodeForThisLesson < episodeNumber)
-    ) {
-      setFurthestEpisodes({
-        ...furthestEpisodes,
-        [lessonName]: episodeNumber
-      })
-    }
-  }, [episodeNumber, furthestEpisodes, lessonName, setFurthestEpisodes])
+    // https://overreacted.io/a-complete-guide-to-useeffect/#making-effects-self-sufficient
+    setFurthestEpisodes(prevFurthestEpisodes => {
+      const furthestEpisodeForThisLesson = prevFurthestEpisodes[lessonName]
+      if (
+        !furthestEpisodeForThisLesson ||
+        (typeof furthestEpisodeForThisLesson !== 'undefined' &&
+          furthestEpisodeForThisLesson < episodeNumber)
+      ) {
+        return {
+          ...prevFurthestEpisodes,
+          [lessonName]: episodeNumber
+        }
+      } else {
+        return prevFurthestEpisodes
+      }
+    })
+  }, [episodeNumber, lessonName, setFurthestEpisodes])
   return <React.Fragment>{children}</React.Fragment>
 }
 
-export default withRouter<GlobalContextProviderProps>(GlobalContextSetter)
+export default GlobalContextSetter
