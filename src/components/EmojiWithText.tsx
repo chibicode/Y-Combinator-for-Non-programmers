ec5545198a0e1a66a9e4122ea127c2f3c241a286
Shu Uesugi <shu@chibicode.com>
2019-03-19 01:29:47 -0700

    Start EmojiWithText

---
+++ src/components/EmojiWithText.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import locale from 'src/lib/locale'
+import letterEmojiMapping from 'src/lib/yc/letterEmojiMapping'
+
+// API: <EmojiWithText size="mdlg" letter="a"> => サンドイッチ <Emoji size="mdlg">...</Emoji>
+const EmojiWithText = ({ letter }) => <></>
--- src/lib/yc/letterEmojiMapping.ts
+++ src/lib/yc/letterEmojiMapping.ts
@@ -29,4 +29,111 @@ const letterEmojiMapping: Record<VariableNames, string> = {
   z: '🍦'
 }
 
+export const emojiLabelMapping = {
+  '🍣': {
+    en: 'sushi',
+    jp: 'お寿司'
+  },
+  '🥪': {
+    en: 'sandwich',
+    jp: 'サンドイッチ'
+  },
+  '🍔': {
+    en: 'hamburger',
+    jp: 'ハンバーガー'
+  },
+  '🍗': {
+    en: 'chicken',
+    jp: 'チキン'
+  },
+  '🍟': {
+    en: 'fried potatoes',
+    jp: 'フライドポテト'
+  },
+  '🍕': {
+    en: 'pizza',
+    jp: 'ピザ'
+  },
+  '🍛': {
+    en: 'curry',
+    jp: 'カレー'
+  },
+  '🌭': {
+    en: 'hotdog',
+    jp: 'ホットドッグ'
+  },
+  '🍚': {
+    en: 'rice',
+    jp: 'ライス'
+  },
+  '🍝': {
+    en: 'spaghetti',
+    jp: 'スパゲッティ'
+  },
+  '🥕': {
+    en: 'carrot',
+    jp: 'にんじん'
+  },
+  '🥗': {
+    en: 'salad',
+    jp: 'サラダ'
+  },
+  '🍆': {
+    en: 'eggplant',
+    jp: 'ナス'
+  },
+  '🥟': {
+    en: 'dumpling',
+    jp: '餃子'
+  },
+  '🥐': {
+    en: 'croissant',
+    jp: 'クロワッサン'
+  },
+  '🥞': {
+    en: 'pancakes',
+    jp: 'パンケーキ'
+  },
+  '🧀': {
+    en: 'cheese',
+    jp: 'チーズ'
+  },
+  '🍇': {
+    en: 'grapes',
+    jp: 'ぶどう'
+  },
+  '🍎': {
+    en: 'apple',
+    jp: 'リンゴ'
+  },
+  '🍒': {
+    en: 'cherry',
+    jp: 'さくらんぼ'
+  },
+  '🍋': {
+    en: 'lemon',
+    jp: 'レモン'
+  },
+  '🍌': {
+    en: 'banana',
+    jp: 'バナナ'
+  },
+  '🍍': {
+    en: 'pineapple',
+    jp: 'パイナップル'
+  },
+  '🎂': {
+    en: 'cake',
+    jp: 'ケーキ'
+  },
+  '🍪': {
+    en: 'cookie',
+    jp: 'クッキー'
+  },
+  '🍦': {
+    en: 'icecream',
+    jp: 'アイスクリーム'
+  }
+}
+
 export default letterEmojiMapping
