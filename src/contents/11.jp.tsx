1fad1faf2b5e8aaaa8efef9bdbcef16d35c68d13
Shu Uesugi <shu@chibicode.com>
2019-04-22 23:06:14 -0700

    Finish episode 8

--- src/contents/11.jp.tsx
+++ src/contents/11.jp.tsx
@@ -10,7 +10,8 @@ import {
   Th,
   Ul,
   UlLi,
-  ExternalLink
+  ExternalLink,
+  Blockquote
 } from 'src/components/ContentTags'
 import Emoji from 'src/components/Emoji'
 import EmojiSeparator from 'src/components/EmojiSeparator'
@@ -21,6 +22,7 @@ import h from 'src/lib/h'
 import { colors } from 'src/lib/theme'
 import EpisodeCardList from 'src/components/EpisodeCardList'
 import H from 'src/components/H'
+import { whatIsComputerScience } from 'src/contents/0.jp'
 
 export default () => (
   <EpisodeCardList
@@ -46,6 +48,104 @@ export default () => (
                 <Strong>ラムダ計算</Strong>
               </ExternalLink>
               」と呼ばれました。シンプルで高性能なこの計算機は、特にプログラミング言語の開発に大きな影響を与えました。
+            </P>
+          </>
+        )
+      },
+      {
+        title: <>コンピューターサイエンス = 計算機科学</>,
+        content: (
+          <>
+            <P>
+              この記事の題名に入っている「
+              <Strong>コンピューターサイエンス</Strong>
+              」という言葉について、最初のページで以下のように説明しました:
+            </P>
+            <Blockquote>{whatIsComputerScience}</Blockquote>
+            <P>
+              実はコンピューターサイエンスは、日本語では「
+              <ExternalLink href="https://ja.wikipedia.org/wiki/%E8%A8%88%E7%AE%97%E6%A9%9F%E7%A7%91%E5%AD%A6">
+                <Strong>計算機科学</Strong>
+              </ExternalLink>
+              」と訳されています。
+            </P>
+            <P>
+              コンピューターというとパソコンやタブレット、スマホ、ロボットを思い浮かべるかもしれませんが、コンピューターは本質的には
+              <Strong>「計算機」</Strong>
+              です。たとえば美しいCGや賢いAIも、大量の計算を短時間で行うことによって実現されています。
+            </P>
+            <EmojiSeparator emojis={['💻', '📱', '🤖']} />
+            <P>
+              だから、
+              <Em>
+                「コンピューターサイエンス = 計算機科学」は、
+                <Strong>「計算機」について考える学問</Strong>なのです
+              </Em>
+              。
+            </P>
+          </>
+        )
+      },
+      {
+        title: (
+          <>ほとんどの計算ができる、最もシンプルな計算機はどんなものだろう？</>
+        ),
+        content: (
+          <>
+            <P>
+              コンピューターサイエンスは、「計算機」について考える学問です。そして学問なので、さまざまな
+              <Strong>問い(命題)</Strong>
+              があります。
+            </P>
+            <EmojiSeparator emojis={['❓', '🤔', '❓']} />
+            <P>
+              ここでは、重要な問いをひとつ見ていきます。その問いとは、以下のようなものです:
+            </P>
+            <P>
+              <Strong>
+                「ほとんどの計算ができる、最もシンプルな計算機はどんなものだろう？」
+              </Strong>
+            </P>
+          </>
+        )
+      },
+      {
+        title: <>?</>,
+        content: (
+          <>
+            <P>
+              先ほど話したように、
+              <H args={{ name: 'bentoBox', skipEmoji: true }} />は
+              <Em>
+                足し算・掛け算・引き算・割り算といった四則演算ができる計算機です
+              </Em>
+              。
+            </P>
+            <P>
+              <H args={{ name: 'bentoBox', skipEmoji: true }} />
+              が面白いのは、
+              <Strong>仕組みがシンプルでありながら高性能である</Strong>
+              ということです。
+              <H args={{ name: 'play' }} />
+              する際のルールも、暗号の法則も、すぐに覚えられるくらい単純です。しかし、
+              <Em>
+                その単純さとは裏腹に、足し算や掛け算といった遥かに複雑な計算を行うことができる
+              </Em>
+              のです。
+            </P>
+            <EmojiSeparator emojis={['➕', '🤨', '✖️']} />
+            <P>
+              一方、
+              <Em>
+                四則演算ができるくらいでは、問いにあったように
+                <Strong>「ほとんどの計算ができる」とは言えない</Strong>
+              </Em>
+              でしょう。もっと難しい計算ができないといけません。
+            </P>
+            <P>
+              というわけで、次のページからは、
+              <H args={{ name: 'bentoBox', skipEmoji: true }} />
+              を使ったさらにハイレベルな計算を紹介していきます。数学的に難しいわけではないので、ご心配なく！
             </P>
           </>
         )
--- src/contents/8.jp.tsx
+++ src/contents/8.jp.tsx
@@ -5,9 +5,7 @@ import {
   P,
   Strong,
   Ul,
-  UlLi,
-  Blockquote,
-  ExternalLink
+  UlLi
 } from 'src/components/ContentTags'
 import EmojiSeparator from 'src/components/EmojiSeparator'
 import EpisodeCardList from 'src/components/EpisodeCardList'
@@ -15,7 +13,7 @@ import { InlineEmojiBoxesForQuestion } from 'src/components/Yc/InlineEmojiBoxes'
 import AER from 'src/components/Yc/AllExpressionRunners'
 import H from 'src/components/H'
 import YesNoButtons from 'src/components/YesNoButtons'
-import { whatIsComputerScience } from 'src/contents/0.jp'
+import YcNextLessonButton from 'src/components/Yc/YcNextLessonButton'
 
 export default () => (
   <EpisodeCardList
@@ -238,7 +236,7 @@ export default () => (
         )
       },
       {
-        title: <>他の計算もできます</>,
+        title: <>引き算や割り算もできます</>,
         content: (
           <>
             <P>
@@ -251,105 +249,29 @@ export default () => (
             <EmojiSeparator emojis={['➖', '🍱️', '➗']} />
             <P>
               つまり、
-              <H args={{ name: 'bentoBox' }} />は<Strong>電卓</Strong>
-              のように使えるのです。ある種の<Strong>「計算機」</Strong>
-              と読んでも過言ではないでしょう。
+              <H args={{ name: 'bentoBox' }} />は<Strong>四則演算</Strong>{' '}
+              (足し算・掛け算・引き算・割り算)ができるのです。
             </P>
           </>
         )
       },
       {
-        title: <>コンピューターサイエンス = 計算機科学</>,
+        title: <>他には何ができるのか？</>,
         content: (
           <>
             <P>
-              この記事の題名に入っている「
-              <Strong>コンピューターサイエンス</Strong>
-              」という言葉について、最初のページで以下のように説明しました:
-            </P>
-            <Blockquote>{whatIsComputerScience}</Blockquote>
-            <P>
-              実はコンピューターサイエンスは、日本語では「
-              <ExternalLink href="https://ja.wikipedia.org/wiki/%E8%A8%88%E7%AE%97%E6%A9%9F%E7%A7%91%E5%AD%A6">
-                <Strong>計算機科学</Strong>
-              </ExternalLink>
-              」と訳されています。
-            </P>
-            <P>
-              コンピューターというとパソコンやタブレット、スマホ、ロボットを思い浮かべるかもしれませんが、本来コンピューターは
-              <Strong>「計算機」</Strong>
-              です。たとえば美しいCGや賢いAIも、大量の「計算」を短時間で行うことによって実現されています。
-            </P>
-            <EmojiSeparator emojis={['💻', '📱', '🤖']} />
-            <P>
-              だから、
+              <H args={{ name: 'bentoBox', short: true }} />
+              に四則演算ができることがわかりましたが、これだと
               <Em>
-                「コンピューターサイエンス = 計算機科学」は、
-                <Strong>「計算機」について考える学問</Strong>なのです
+                <Strong>普通の電卓</Strong>とさほど変わりません
               </Em>
-              。
+              よね。
             </P>
-          </>
-        )
-      },
-      {
-        title: (
-          <>ほとんどの計算ができる、最もシンプルな計算機はどんなものだろう？</>
-        ),
-        content: (
-          <>
+            <EmojiSeparator emojis={['🍱', '🤔', '🍱']} />
             <P>
-              コンピューターサイエンスは計算機について考える学問で、さまざまな
-              <Em>問い(命題)</Em>があります。そのうちのひとつがこちら:
+              他に何かできることはあるのでしょうか？次のページで見ていきます！
             </P>
-            <P>
-              <Strong>
-                「ほとんどの計算ができる、最もシンプルな計算機はどんなものだろう？」
-              </Strong>
-            </P>
-            <EmojiSeparator emojis={['❓', '🤔', '❓']} />
-            <P>
-              <Strong>
-                実は、この問いの答えが
-                <H args={{ name: 'bentoBox', short: true }} />
-                なのです。
-              </Strong>
-            </P>
-            <EmojiSeparator emojis={['🍱', '😲', '🍱']} />
-            <P>
-              先ほど話したように、
-              <H args={{ name: 'bentoBox', skipEmoji: true }} />は
-              <Em>
-                足し算・掛け算・引き算・割り算といった四則演算ができる計算機です
-              </Em>
-              。
-            </P>
-            <P>
-              <H args={{ name: 'bentoBox', skipEmoji: true }} />
-              が面白いのは、
-              <Strong>仕組みがシンプルでありながら高性能である</Strong>
-              ということです。
-              <H args={{ name: 'play' }} />
-              する際のルールも、暗号の法則も、すぐに覚えられるくらい単純です。しかし、
-              <Em>
-                その単純さとは裏腹に、足し算や掛け算といった遥かに複雑な計算を行うことができる
-              </Em>
-              のです。
-            </P>
-            <EmojiSeparator emojis={['➕', '🤨', '✖️']} />
-            <P>
-              一方、
-              <Em>
-                四則演算ができるくらいでは、問いにあったように
-                <Strong>「ほとんどの計算ができる」とは言えない</Strong>
-              </Em>
-              でしょう。もっと難しい計算ができないといけません。
-            </P>
-            <P>
-              というわけで、次のページからは、
-              <H args={{ name: 'bentoBox', skipEmoji: true }} />
-              を使ったさらにハイレベルな計算を紹介していきます。数学的に難しいわけではないので、ご心配なく！
-            </P>
+            <YcNextLessonButton nextEpisodeNumber={9} />
           </>
         )
       }
