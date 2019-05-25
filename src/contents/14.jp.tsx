2940a954d6b2a7ddf926d9c22ae44a100ad392a7
Shu Uesugi <shu@chibicode.com>
2019-05-24 20:07:07 -0700

    Update 14.jp.tsx

--- src/contents/14.jp.tsx
+++ src/contents/14.jp.tsx
@@ -8,16 +8,32 @@ export default () => (
   <EpisodeCardList
     cards={[
       {
-        title: <>条件分岐の弁当箱</>,
+        title: (
+          <>
+            条件分岐の弁当箱
+            <H args={{ name: 'bentoBox', skipEmoji: true }} />
+          </>
+        ),
         content: (
           <>
             <P>
               <H args={{ name: 'episodeWelcomeText' }} />
-              今回は、以前紹介した「<Strong>条件分岐の弁当箱</Strong>
+              今回は、以前紹介した「
+              <Strong>
+                条件分岐の
+                <H args={{ name: 'bentoBox', skipEmoji: true }} />
+              </Strong>
               」を省略表記していきます。まずは、復習から:
             </P>
             {AER.nlxe}
             <H args={{ name: 'isZeroReview' }} />
+          </>
+        )
+      },
+      {
+        title: <>条件分岐の省略表記</>,
+        content: (
+          <>
             <P>では、これを省略表記するとどうなるでしょう？</P>
             {AER.hvdn}
           </>
