9beaf5490949b99e9a5268045044a3f6554c293c
Shu Uesugi <shu@chibicode.com>
2019-05-27 15:25:08 -0700

    ConditionalContext

--- src/components/ConditionalExpressionBox.tsx
+++ src/components/ConditionalExpressionBox.tsx
@@ -5,6 +5,7 @@ import FlexCenter from 'src/components/FlexCenter'
 import ExpressionBox from 'src/components/ExpressionBox'
 import { ConditionalExpression } from 'src/types/ExpressionTypes'
 import ConditionalBorder from 'src/components/ConditionalBorder'
+import ConditionalContext from 'src/components/ConditionalContext'
 
 interface ConditionalExpressionBoxProps {
   expression: ConditionalExpression
@@ -14,38 +15,42 @@ const ConditionalExpressionBox = ({
   expression
 }: ConditionalExpressionBoxProps) => {
   return (
-    <Flex
-      css={css`
-        flex-direction: column;
-        flex: 1;
-        position: relative;
-      `}
+    <ConditionalContext.Provider
+      value={{ conditionalActive: expression.state !== 'default' }}
     >
-      <FlexCenter
+      <Flex
         css={css`
+          flex-direction: column;
+          flex: 1;
           position: relative;
         `}
       >
-        <ConditionalBorder addBottom type="falseCase" />
-        <ExpressionBox expression={expression.falseCase} />
-      </FlexCenter>
-      <FlexCenter
-        css={css`
-          position: relative;
-        `}
-      >
-        <ConditionalBorder addBottom type="condition" />
-        <ExpressionBox expression={expression.condition} />
-      </FlexCenter>
-      <FlexCenter
-        css={css`
-          position: relative;
-        `}
-      >
-        <ConditionalBorder type="trueCase" />
-        <ExpressionBox expression={expression.trueCase} />
-      </FlexCenter>
-    </Flex>
+        <FlexCenter
+          css={css`
+            position: relative;
+          `}
+        >
+          <ConditionalBorder addBottom type="falseCase" />
+          <ExpressionBox expression={expression.falseCase} />
+        </FlexCenter>
+        <FlexCenter
+          css={css`
+            position: relative;
+          `}
+        >
+          <ConditionalBorder addBottom type="condition" />
+          <ExpressionBox expression={expression.condition} />
+        </FlexCenter>
+        <FlexCenter
+          css={css`
+            position: relative;
+          `}
+        >
+          <ConditionalBorder type="trueCase" />
+          <ExpressionBox expression={expression.trueCase} />
+        </FlexCenter>
+      </Flex>
+    </ConditionalContext.Provider>
   )
 }
 
