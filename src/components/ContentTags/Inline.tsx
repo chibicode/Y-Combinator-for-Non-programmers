47d47c24081dabe62866d43f1f21b60ffd5100d8
Shu Uesugi <shu@chibicode.com>
2019-05-29 20:34:13 -0700

    Update mix

--- src/components/ContentTags/Inline.tsx
+++ src/components/ContentTags/Inline.tsx
@@ -26,7 +26,7 @@ const HighlightContext = React.createContext<HighlightContextProps>(
 
 const mix = (color: CardProps['color'], base: string) =>
   Color(base)
-    .mix(Color(backgroundColor(color)), 0.5)
+    .mix(Color(backgroundColor(color)), 0.4)
     .hsl()
     .string()
 
