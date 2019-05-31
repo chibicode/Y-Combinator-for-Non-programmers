d864b179d0ff3dfca5b9ff6a71ead98e89caf755
Shu Uesugi <shu@chibicode.com>
2019-05-30 21:32:43 -0700

    Start episode 15

--- src/contents/15.jp.tsx
+++ src/contents/15.jp.tsx
@@ -1,20 +1,47 @@
 import React from 'react'
 import EpisodeCardList from 'src/components/EpisodeCardList'
-import { P } from 'src/components/ContentTags'
+import { P, Strong, Ol, OlLi, Em } from 'src/components/ContentTags'
 import H from 'src/components/H'
 import AER from 'src/components/AER'
+import EmojiSeparator from 'src/components/EmojiSeparator'
 
 export default () => (
   <EpisodeCardList
     cards={[
       {
-        title: <>?</>,
+        title: <>これが上級最後！</>,
         content: (
           <>
             <P>
               <H args={{ name: 'episodeWelcomeText' }} />
+              これが上級最後のページです(次のページはエピローグです)。
             </P>
-            {AER.zxux}
+            <EmojiSeparator emojis={['🎉', '🤗', '🎉']} />
+            <P>
+              前回話した通り、この上級最終回では 「
+              <Strong>ある条件を満たすまで、何かを繰り返す弁当箱</Strong>」
+              の解説をします。
+            </P>
+            <EmojiSeparator emojis={['🤔', '🍱', '❓']} />
+            <P>
+              できるだけ分かりやすく解説するために、以下の順番で解説していきます。
+            </P>
+            <Ol>
+              <OlLi>
+                まず、
+                <Strong>実際には存在しない架空の弁当箱の機能を使い</Strong>
+                、「<Em>ある条件を満たすまで、何かを繰り返す弁当箱</Em>
+                」を作ってみます。
+              </OlLi>
+              <OlLi>
+                その後に、<Strong>従来の弁当箱の機能だけを使い</Strong>
+                、「<Em>ある条件を満たすまで、何かを繰り返す弁当箱</Em>
+                」を作ってみます。
+              </OlLi>
+            </Ol>
+            <P>
+              「架空の機能って何？」とお考えかもしれません。さっそく説明していきます！
+            </P>
           </>
         )
       }
