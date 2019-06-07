60b87fde6ed74784cb1f4faf1d9e12975cb5f713
Shu Uesugi <shu@chibicode.com>
2019-06-07 11:33:18 -0700

    Fix viewport

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import Document, { Html, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { lang } from 'src/lib/locale'
 
 class MyDocument extends Document {
   public render() {
     return (
       <Html lang={lang}>
+        <Head />
         <body>
           <Main />
           <NextScript />
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -12,7 +12,6 @@ const Page = ({ children }: { children: React.ReactNode }) => (
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        key="viewport"
       />
       <meta property="og:type" content="article" />
       <meta property="og:locale" content={ogLocale} />
