ea04866c659f4c060ecae4f2d59e525e2dc281eb
Shu Uesugi <shu@chibicode.com>
2019-07-01 15:29:43 -0700

    Blank number

---
+++ src/components/BlankNumber.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const BlankNumber = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 36 36" {...props}>
+    <path
+      fill="#3B88C3"
+      d="M36 32c0 2.209-1.791 4-4 4H4c-2.209 0-4-1.791-4-4V4c0-2.209 1.791-4 4-4h28c2.209 0 4 1.791 4 4v28z"
+    />
+  </svg>
+)
+
+export default BlankNumber
