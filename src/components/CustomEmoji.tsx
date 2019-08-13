50fcfceabbd5fb13feaca0e3c0a344513fc4aea6
Shu Uesugi <shu@chibicode.com>
2019-08-13 07:09:22 -0700

    Add questionFood

--- src/components/CustomEmoji.tsx
+++ src/components/CustomEmoji.tsx
@@ -4,6 +4,7 @@ import MinusOneSvg from 'src/components/MinusOneSvg'
 import BlankNumberSvg from 'src/components/BlankNumberSvg'
 import PlusMinusSvg from 'src/components/PlusMinusSvg'
 import VerticalDotDotDotSvg from 'src/components/VerticalDotDotDotSvg'
+import QuestionFoodSvg from 'src/components/QuestionFoodSvg'
 import HorizontalDotDotDotSvg from 'src/components/HorizontalDotDotDotSvg'
 import HorizontalDotDotDotRedSvg from 'src/components/HorizontalDotDotDotRedSvg'
 import Emoji, { EmojiProps } from 'src/components/Emoji'
@@ -20,6 +21,7 @@ const CustomEmoji = ({
     | 'verticalDotDotDot'
     | 'horizontalDotDotDot'
     | 'horizontalDotDotDotRed'
+    | 'questionFood'
 }) => {
   const Component = {
     plusOne: PlusOneSvg,
@@ -28,7 +30,8 @@ const CustomEmoji = ({
     plusMinus: PlusMinusSvg,
     verticalDotDotDot: VerticalDotDotDotSvg,
     horizontalDotDotDot: HorizontalDotDotDotSvg,
-    horizontalDotDotDotRed: HorizontalDotDotDotRedSvg
+    horizontalDotDotDotRed: HorizontalDotDotDotRedSvg,
+    questionFood: QuestionFoodSvg
   }[type]
   return <Emoji {...emojiProps} customSvg={<Component />} />
 }
