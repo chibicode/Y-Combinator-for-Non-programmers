15de7bf655055d771bef2f4556b63d4b66a5fd95
Shu Uesugi <shu@chibicode.com>
2019-08-14 23:48:03 -0700

    Rewrite 7

--- src/contents/7.v2.jp.tsx
+++ src/contents/7.v2.jp.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import EpisodeCardList from 'src/components/EpisodeCardList'
-import { P, Em, Strong } from 'src/components/ContentTags'
+import { P, Em } from 'src/components/ContentTags'
 import CustomEmoji from 'src/components/CustomEmoji'
 import BubbleQuotes from 'src/components/BubbleQuotes'
 // import EmojiNumber from 'src/components/EmojiNumber'
@@ -10,7 +10,6 @@ import H from 'src/components/H'
 import * as R from 'src/components/Runners'
 // import EmojiWithText from 'src/components/EmojiWithText'
 // import EmojiForLetter from 'src/components/EmojiForLetter'
-import ExpressionRunnerSeparator from 'src/components/ExpressionRunnerSeparator'
 // import NextLessonButton from 'src/components/NextLessonButton'
 
 export default () => (
@@ -54,78 +53,10 @@ export default () => (
               run and convert you'll get 2. Then say essentially this is the
               same as math box 1 + 1. Then also do the same with 2 + 1.
             </P>
-            <BubbleQuotes
-              quotes={[
-                {
-                  type: 'dog',
-                  children: (
-                    <>
-                      <P>
-                        はい。
-                        <H args={{ name: 'lookAtThisBentoBox' }} />。
-                      </P>
-                      <P>
-                        <Em>
-                          それぞれの <CustomEmoji type="questionFoodGrey" />{' '}
-                          には、何らかの料理が入ります。
-                        </Em>
-                      </P>
-                    </>
-                  )
-                }
-              ]}
-            />
             <R.Envj>
               <CustomEmoji type="questionFoodGrey" /> には何らかの料理が入ります
             </R.Envj>
-            <BubbleQuotes
-              quotes={[
-                {
-                  type: 'thinking',
-                  children: (
-                    <>
-                      <P>何だか複雑な弁当箱だなあ…</P>
-                    </>
-                  )
-                },
-                {
-                  type: 'dog',
-                  children: (
-                    <>
-                      <P>
-                        そうですね。そして、
-                        <Strong>
-                          それぞれの <CustomEmoji type="questionFoodGrey" />{' '}
-                          に、ある法則に基づいて料理を当てはめる
-                        </Strong>
-                        と…
-                      </P>
-                      <P>
-                        ぼくはこの弁当箱を{' '}
-                        <Em>
-                          <CustomEmoji type="plusOne" />{' '}
-                          の計算箱に変換することができる
-                        </Em>
-                        んです。
-                      </P>
-                    </>
-                  )
-                }
-              ]}
-            />
-            <R.Envj>
-              <Strong>
-                それぞれの <CustomEmoji type="questionFoodGrey" />{' '}
-                に、ある法則に
-                <br />
-                基づいて料理を当てはめて…
-              </Strong>
-            </R.Envj>
-            <ExpressionRunnerSeparator />
-            <R.Biit>
-              <H args={{ name: 'convert', type: 'toMathBox' }} /> すると、
-              <CustomEmoji type="plusOne" /> になる
-            </R.Biit>
+            <R.Biit></R.Biit>
           </>
         )
       },
@@ -136,45 +67,7 @@ export default () => (
             <R.Pbop>
               <CustomEmoji type="questionFoodRed" /> には何らかの料理が入ります
             </R.Pbop>
-            <BubbleQuotes
-              quotes={[
-                {
-                  type: 'dog',
-                  children: (
-                    <>
-                      <P>
-                        そして、
-                        <Strong>
-                          それぞれの <CustomEmoji type="questionFoodRed" />{' '}
-                          に、ある法則に基づいて料理を当てはめる
-                        </Strong>
-                        と…
-                      </P>
-                      <P>
-                        ぼくはこの弁当箱を{' '}
-                        <Em>
-                          <CustomEmoji type="minusOne" />{' '}
-                          の計算箱に変換することができる
-                        </Em>
-                        んです。
-                      </P>
-                    </>
-                  )
-                }
-              ]}
-            />
-            <R.Pbop>
-              <Strong>
-                それぞれの <CustomEmoji type="questionFoodRed" /> に、ある法則に
-                <br />
-                基づいて料理を当てはめて…
-              </Strong>
-            </R.Pbop>
-            <ExpressionRunnerSeparator />
-            <R.Uxbl>
-              <H args={{ name: 'convert', type: 'toMathBox' }} /> すると、
-              <CustomEmoji type="minusOne" /> になる
-            </R.Uxbl>
+            <R.Uxbl></R.Uxbl>
           </>
         )
       },
