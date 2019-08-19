45deffab54f4d8fb630e62b042606d0f98d6ea96
Shu Uesugi <shu@chibicode.com>
2019-08-19 14:20:50 -0700

    Update date.tsx

--- src/lib/date.tsx
+++ src/lib/date.tsx
@@ -2,8 +2,8 @@ import locale from 'src/lib/locale'
 import { DateTime } from 'luxon'
 
 export const date = {
-  jp: DateTime.fromISO('2019-08-26T00:00:00Z'),
-  en: DateTime.fromISO('2019-09-26T00:00:00Z')
+  jp: DateTime.fromISO('2019-09-01T00:00:00Z'),
+  en: DateTime.fromISO('2019-10-01T00:00:00Z')
 }[locale]
 
 export const dateString = {
