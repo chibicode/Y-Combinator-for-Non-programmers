7d1400dc49d61401143afe76e9f1dd165c277f19
Shu Uesugi <shu@chibicode.com>
2019-08-26 17:13:18 -0700

    Start 11

--- src/contents/11.v2.jp.tsx
+++ src/contents/11.v2.jp.tsx
@@ -1,4 +1,18 @@
 import React from 'react'
 import EpisodeCardList from 'src/components/EpisodeCardList'
+import { P } from 'src/components/ContentTags'
 
-export default () => <EpisodeCardList cards={[]} />
+export default () => (
+  <EpisodeCardList
+    cards={[
+      {
+        title: <>上級編へようこそ！</>,
+        content: (
+          <>
+            <P></P>
+          </>
+        )
+      }
+    ]}
+  />
+)
