6ed7f378bead2d8d8d9b6e0bd0f9441dd3ba55c0
Shu Uesugi <shu@chibicode.com>
2019-08-28 19:09:39 -0700

    Start 12

--- src/contents/12.v2.jp.tsx
+++ src/contents/12.v2.jp.tsx
@@ -1,4 +1,76 @@
 import React from 'react'
 import EpisodeCardList from 'src/components/EpisodeCardList'
+import {
+  P,
+  Strong
+  // Hr,
+  // Em,
+  // Ul,
+  // UlLi,
+  // InlineHeader
+} from 'src/components/ContentTags'
+// import H from 'src/components/H'
+import BubbleQuotes from 'src/components/BubbleQuotes'
+// import EmojiNumber from 'src/components/EmojiNumber'
+import EmojiSeparator from 'src/components/EmojiSeparator'
+// import CustomEmoji from 'src/components/CustomEmoji'
+import ExpressionRunnerSeparator from 'src/components/ExpressionRunnerSeparator'
+// import ExpressionRunnerCaptionOnly from 'src/components/ExpressionRunnerCaptionOnly'
+import Emoji from 'src/components/Emoji'
+// import TwoColGrid from 'src/components/TwoColGrid'
+// import InlineBorder from 'src/components/InlineBorder'
+import * as R from 'src/components/Runners'
+// import NextLessonButton from 'src/components/NextLessonButton'
+// import YesNoButtons from 'src/components/YesNoButtons'
+// import { TwoPlusFour, Conclusion } from 'src/contents/2.v2.jp'
 
-export default () => <EpisodeCardList cards={[]} />
+export default () => (
+  <EpisodeCardList
+    cards={[
+      {
+        title: <>条件分岐の再現</>,
+        content: (
+          <>
+            <P>
+              村人たちは与えられたヒントをもとに、「
+              <Strong>条件分岐の機能</Strong> <Emoji>↕️</Emoji>
+              」を弁当箱で再現しようとしていました。
+            </P>
+            <R.Rhoa>
+              「<Strong>条件分岐の機能</Strong> <Emoji>↕️</Emoji>」を…
+            </R.Rhoa>
+            <ExpressionRunnerSeparator />
+            <EmojiSeparator
+              emojis={['❓', '🍱', '❓']}
+              description={
+                <>
+                  <Strong>弁当箱で再現できる？</Strong>
+                </>
+              }
+            />
+            <BubbleQuotes
+              quotes={[
+                {
+                  type: 'thinking',
+                  children: (
+                    <>
+                      <P>うーん…難しいなあ…</P>
+                    </>
+                  )
+                },
+                {
+                  type: 'saya',
+                  children: (
+                    <>
+                      <P>サヤもちょっと考えてみた！これを見てくれる？</P>
+                    </>
+                  )
+                }
+              ]}
+            />
+          </>
+        )
+      }
+    ]}
+  />
+)
