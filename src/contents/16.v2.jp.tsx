25362c9c37de58f8f87b56e555c2c2d7901aac58
Shu Uesugi <shu@chibicode.com>
2019-09-04 01:46:03 -0700

    Start episode 16

--- src/contents/16.v2.jp.tsx
+++ src/contents/16.v2.jp.tsx
@@ -1,13 +1,68 @@
 import React from 'react'
 import EpisodeCardList from 'src/components/EpisodeCardList'
+import { P, Strong } from 'src/components/ContentTags'
+import EmojiSeparator from 'src/components/EmojiSeparator'
+import BubbleQuotes from 'src/components/BubbleQuotes'
 
 export default () => (
   <EpisodeCardList
-    cards={
-      [
-        // TODO: Don't use ラムダ村 until the very end - b/c we need to talk about computers.
-        // But end with what happened to ラムダ村
-      ]
-    }
+    cards={[
+      {
+        title: <>これが最後のページです！</>,
+        content: (
+          <>
+            <P>
+              これが最後のページです。ここまで読んでくださり、ありがとうございます！
+            </P>
+            <EmojiSeparator
+              emojis={['🎉', '🤗', '🎉']}
+              description={<>お疲れ様でした！</>}
+            />
+            <P>
+              さて、前回の最後に、ラムダ村の村人は次のような質問をしました。
+            </P>
+            <BubbleQuotes
+              quotes={[
+                {
+                  type: 'thinking',
+                  children: (
+                    <>
+                      <P>
+                        <Strong>弁当箱にできない計算はあるのかな？</Strong>
+                      </P>
+                    </>
+                  )
+                }
+              ]}
+            />
+            <P>
+              この質問に答えるためには、「
+              <Strong>
+                そもそも弁当箱というアイデアはどうやって生まれたのか
+              </Strong>
+              」という質問にまず答える必要があります。
+            </P>
+            <EmojiSeparator
+              emojis={['❓', '🍱', '❓']}
+              description={
+                <>
+                  そもそも、弁当箱というアイデアは
+                  <br />
+                  どうやって生まれたのか？
+                </>
+              }
+            />
+            <P>
+              そのためには、
+              <Strong>ラムダ村の世界ではなく、現実世界の歴史</Strong>
+              を語る必要があります。
+            </P>
+            <P>
+              というわけで、これから現実世界の歴史の話をしますが、お付き合いいただけると嬉しいです。ラムダ村の話からはいったん離れますが、このページの最後で戻ってきます。
+            </P>
+          </>
+        )
+      }
+    ]}
   />
 )
