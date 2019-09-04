b8398c8da9b5c0a3d45eec88ad9614ac353f2552
Shu Uesugi <shu@chibicode.com>
2019-09-04 00:24:51 -0700

    todo for 16

--- src/contents/16.v2.jp.tsx
+++ src/contents/16.v2.jp.tsx
@@ -1,4 +1,13 @@
 import React from 'react'
 import EpisodeCardList from 'src/components/EpisodeCardList'
 
-export default () => <EpisodeCardList cards={[]} />
+export default () => (
+  <EpisodeCardList
+    cards={
+      [
+        // TODO: Don't use ラムダ村 until the very end - b/c we need to talk about computers.
+        // But end with what happened to ラムダ村
+      ]
+    }
+  />
+)
