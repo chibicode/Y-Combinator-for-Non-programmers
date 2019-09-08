82b12032d1853e5e2e08d52c37900816f7f4ce0a
Shu Uesugi <shu@chibicode.com>
2019-09-07 20:02:44 -0700

    Fix progress bar logic

--- src/components/ExpressionRunnerPrecomputed.tsx
+++ src/components/ExpressionRunnerPrecomputed.tsx
@@ -157,7 +157,7 @@ const ExpressionRunnerPrecomputed = ({
       !isPlaying &&
       currentIndex > 0)
   const progessBarVisible =
-    !hidePlayButton && (isPlaying || atLeastOneStepTaken)
+    !hidePlayButton && !skipToTheEnd && (isPlaying || atLeastOneStepTaken)
 
   return (
     <ExpressionRunnerContext.Provider
