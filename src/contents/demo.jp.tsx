e150fc81dd661c0edfbb6300292f2a8c2f3db8bc
Shu Uesugi <shu@chibicode.com>
2019-09-18 09:38:41 -0700

    Continue with demo page

--- src/contents/demo.jp.tsx
+++ src/contents/demo.jp.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 import { P, Hr } from 'src/components/ContentTags'
 import EpisodeCardList from 'src/components/EpisodeCardList'
 import ExpressionRunnerSeparator from 'src/components/ExpressionRunnerSeparator'
+import EmojiSeparator from 'src/components/EmojiSeparator'
+import Emoji from 'src/components/Emoji'
+import CustomEmoji from 'src/components/CustomEmoji'
 import H from 'src/components/H'
 import * as R from 'src/components/Runners'
+import { BasicRules, Unmatched } from 'src/contents/4.jp'
+import { ThreeRowRules, Beginner5Rules } from 'src/contents/5.jp'
 
 const DemoCardList = () => (
   <EpisodeCardList
@@ -17,6 +22,21 @@ const DemoCardList = () => (
             <P>
               このページはデモ用に作ったページです。わたしがデモを行いながらこのページを使うので、説明は一切書いていません。
             </P>
+          </>
+        )
+      },
+      {
+        title: <>パート1: 計算箱の説明</>,
+        content: (
+          <>
+            <EmojiSeparator
+              nodes={[
+                <CustomEmoji type="mathBox" />,
+                <CustomEmoji type="mathBox" />,
+                <CustomEmoji type="mathBox" />
+              ]}
+              description={<>計算箱</>}
+            />
           </>
         )
       },
@@ -83,6 +103,61 @@ const DemoCardList = () => (
             <R.Sucz></R.Sucz>
             <ExpressionRunnerSeparator />
             <R.Xlgb></R.Xlgb>
+          </>
+        )
+      },
+      {
+        title: <>パート2: 弁当箱の説明</>,
+        content: (
+          <>
+            <EmojiSeparator
+              nodes={[<Emoji>🍱</Emoji>, <Emoji>🍱</Emoji>, <Emoji>🍱</Emoji>]}
+              description={<>弁当箱</>}
+            />
+          </>
+        )
+      },
+      {
+        title: <>2段の弁当箱</>,
+        content: (
+          <>
+            <R.Wunw />
+            <R.Hluq />
+            <R.Jbam />
+            <R.Zuus />
+          </>
+        )
+      },
+      {
+        title: <>弁当箱の法則</>,
+        type: 'summary',
+        content: (
+          <>
+            <BasicRules includeFuncUnbound />
+            <Hr />
+            <Unmatched />
+          </>
+        )
+      },
+      {
+        title: <>3段の弁当箱</>,
+        content: (
+          <>
+            <R.Beiz />
+            <R.Hdxc />
+            <R.Thkn />
+            <R.Cuwg />
+          </>
+        )
+      },
+      {
+        type: 'summary',
+        title: <>3段の弁当箱の法則</>,
+        content: (
+          <>
+            <ThreeRowRules />
+            <Hr />
+            <Beginner5Rules />
           </>
         )
       }
