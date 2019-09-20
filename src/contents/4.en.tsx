5ec95a51458428b7f68c079936c9840137cf1213
Shu Uesugi <shu@chibicode.com>
2019-09-20 14:29:51 -0700

    Recover reused code

--- src/contents/4.en.tsx
+++ src/contents/4.en.tsx
@@ -1,4 +1,104 @@
 import React from 'react'
 import EpisodeCardList from 'src/components/EpisodeCardList'
+import { P, Strong, InlineHeader } from 'src/components/ContentTags'
+import BottomRightBadge from 'src/components/BottomRightBadge'
+import Emoji from 'src/components/Emoji'
+import * as R from 'src/components/Runners'
+import ExpressionRunnerSeparator from 'src/components/ExpressionRunnerSeparator'
+
+export const BasicRules = ({
+  includeFuncUnbound
+}: {
+  includeFuncUnbound?: boolean
+}) => (
+  <>
+    <P>
+      1. <InlineHeader>Add labels:</InlineHeader>{' '}
+      <BottomRightBadge inline bottomRightBadgeType="callArg" />{' '}
+      <BottomRightBadge inline bottomRightBadgeType="funcArg" />{' '}
+      {includeFuncUnbound && (
+        <>
+          <BottomRightBadge inline bottomRightBadgeType="funcUnbound" />{' '}
+        </>
+      )}
+      <BottomRightBadge inline bottomRightBadgeType="funcBound" />
+    </P>
+    <R.Zzxj>
+      <InlineHeader>Add labels:</InlineHeader>{' '}
+      <BottomRightBadge inline bottomRightBadgeType="callArg" />{' '}
+      <BottomRightBadge inline bottomRightBadgeType="funcArg" />{' '}
+      {includeFuncUnbound && (
+        <>
+          <BottomRightBadge inline bottomRightBadgeType="funcUnbound" />{' '}
+        </>
+      )}
+      <BottomRightBadge inline bottomRightBadgeType="funcBound" />
+    </R.Zzxj>
+    <P>
+      2. <InlineHeader>Find matches:</InlineHeader>{' '}
+      <BottomRightBadge inline bottomRightBadgeType="funcArg" />{' '}
+      <BottomRightBadge inline bottomRightBadgeType="funcBound" />{' '}
+      <Emoji>✅</Emoji>
+    </P>
+    <R.Keck>
+      <InlineHeader>Find matches:</InlineHeader>{' '}
+      <BottomRightBadge inline bottomRightBadgeType="funcArg" />{' '}
+      <BottomRightBadge inline bottomRightBadgeType="funcBound" />{' '}
+      <Emoji>✅</Emoji>
+    </R.Keck>
+    <P>
+      3. <InlineHeader>Copy:</InlineHeader>{' '}
+      <BottomRightBadge inline bottomRightBadgeType="callArg" />{' '}
+      <Emoji>↘️</Emoji>{' '}
+      <BottomRightBadge inline bottomRightBadgeType="funcBound" />
+    </P>
+    <R.Qoms>
+      <InlineHeader>Copy:</InlineHeader>{' '}
+      <BottomRightBadge inline bottomRightBadgeType="callArg" />{' '}
+      <Emoji>↘️</Emoji>{' '}
+      <BottomRightBadge inline bottomRightBadgeType="funcBound" />
+    </R.Qoms>
+    <P>
+      4. <InlineHeader>Remove:</InlineHeader> <Emoji>💥</Emoji>{' '}
+      <BottomRightBadge inline bottomRightBadgeType="callArg" />{' '}
+      <BottomRightBadge inline bottomRightBadgeType="funcArg" />
+    </P>
+    <R.Mhgm>
+      <InlineHeader>Remove:</InlineHeader> <Emoji>💥</Emoji>{' '}
+      <BottomRightBadge inline bottomRightBadgeType="callArg" />{' '}
+      <BottomRightBadge inline bottomRightBadgeType="funcArg" />
+    </R.Mhgm>
+    <ExpressionRunnerSeparator />
+    <R.Osqo />
+  </>
+)
+
+export const Unmatched = () => (
+  <>
+    <P>
+      If none of <BottomRightBadge inline bottomRightBadgeType="funcArg" />
+      ’s and <BottomRightBadge inline bottomRightBadgeType="funcBound" />
+      ’s match,{' '}
+      <Strong>
+        don’t do the copy step and simply remove{' '}
+        <BottomRightBadge inline bottomRightBadgeType="callArg" />
+        ’s and <BottomRightBadge inline bottomRightBadgeType="funcArg" />
+        ’s
+      </Strong>
+      .
+    </P>
+    <R.Jwzh>
+      If none of <BottomRightBadge inline bottomRightBadgeType="funcArg" />
+      ’s and <BottomRightBadge inline bottomRightBadgeType="funcBound" />
+      ’s match…
+    </R.Jwzh>
+    <ExpressionRunnerSeparator />
+    <R.Knhw>
+      Simply remove <BottomRightBadge inline bottomRightBadgeType="callArg" />
+      ’s and <BottomRightBadge inline bottomRightBadgeType="funcArg" />
+      ’s
+    </R.Knhw>
+  </>
+)
 
 export default () => <EpisodeCardList underConstruction cards={[]} />
--- src/contents/5.en.tsx
+++ src/contents/5.en.tsx
@@ -1,4 +1,93 @@
 import React from 'react'
 import EpisodeCardList from 'src/components/EpisodeCardList'
+import {
+  P,
+  Em,
+  Strong,
+  Ol,
+  OlLi,
+  InlineHeader,
+  Hr
+} from 'src/components/ContentTags'
+import H from 'src/components/H'
+import InlinePrioritiesLabel from 'src/components/InlinePrioritiesLabel'
+import BottomRightBadge from 'src/components/BottomRightBadge'
+import * as R from 'src/components/Runners'
+
+export const ThreeRowRules = () => (
+  <>
+    <P>
+      <InlineHeader>
+        Here’s how to solve a lunchbox puzzle with at least 3 rows.
+      </InlineHeader>
+    </P>
+    <Ol>
+      <OlLi>
+        <Em>
+          First, start with the pair of{' '}
+          <InlinePrioritiesLabel>1</InlinePrioritiesLabel>’s.
+        </Em>
+      </OlLi>
+      <OlLi>
+        <Em>
+          Label the center item on the bottom row as{' '}
+          <BottomRightBadge inline bottomRightBadgeType="funcUnbound" />, and
+          you can ignore this for the rest of the iteration.
+        </Em>
+      </OlLi>
+      <OlLi>
+        <Em>
+          After finishing the pair of{' '}
+          <InlinePrioritiesLabel>1</InlinePrioritiesLabel>’s,
+          <InlinePrioritiesLabel>2</InlinePrioritiesLabel> will become{' '}
+          <InlinePrioritiesLabel>1</InlinePrioritiesLabel>. Then, repeat.
+        </Em>
+      </OlLi>
+    </Ol>
+    <R.Vdhd>Lunchbox with 3 rows</R.Vdhd>
+  </>
+)
+
+export const Beginner5Rules = () => (
+  <>
+    <P>
+      <InlineHeader>Additional Rule 1:</InlineHeader> If there are multiple
+      pairs of <InlinePrioritiesLabel>1</InlinePrioritiesLabel>’s,{' '}
+      <Strong>start with the leftmost pair</Strong>.
+    </P>
+    <R.Hykj>
+      <H args={{ name: 'startWithLeftMostOneCaption' }} />
+    </R.Hykj>
+    <P>
+      <InlineHeader>Note:</InlineHeader>{' '}
+      <Em>
+        If there’s no <InlinePrioritiesLabel>1</InlinePrioritiesLabel>’s on the
+        leftmost edge of a lunchbox, then start with the inner pair of{' '}
+        <InlinePrioritiesLabel>1</InlinePrioritiesLabel>’s.
+      </Em>
+    </P>
+    <R.Myjz>
+      If there’s no <InlinePrioritiesLabel>1</InlinePrioritiesLabel> on the
+      <br />
+      leftmost edge of a lunchbox, then start with
+      <br />
+      the inner pair of
+      <InlinePrioritiesLabel>1</InlinePrioritiesLabel>’s
+    </R.Myjz>
+    <Hr />
+    <P>
+      <InlineHeader>Additional Rule 2:</InlineHeader> If you can’t start with{' '}
+      <InlinePrioritiesLabel>1</InlinePrioritiesLabel>’s because there’s only
+      one item on the bottom row,{' '}
+      <Strong>
+        start with the pair of <InlinePrioritiesLabel>2</InlinePrioritiesLabel>
+        ’s.
+      </Strong>
+    </P>
+    <R.Zemy>
+      <H args={{ name: 'startWithTwoCaption' }} />
+    </R.Zemy>
+  </>
+)
 
 export default () => <EpisodeCardList underConstruction cards={[]} />
