65a0f63864d881ba61463ac72bfd2f4f93d65167
Shu Uesugi <shu@chibicode.com>
2019-09-21 14:52:58 -0700

    Larger space for H3

--- src/components/ContentTags/Headings.tsx
+++ src/components/ContentTags/Headings.tsx
@@ -3,7 +3,7 @@ import { colors, fontSizes, spaces, ns } from 'src/lib/theme'
 
 export const H3 = styled.h3`
   color: ${colors('grey900')};
-  margin: 0 0 ${spaces(0.75)};
+  margin: 0 0 ${spaces(1)};
   font-size: ${fontSizes(1.2)};
 
   ${ns} {
